0b5fa2828fe0aebc3afd5f557a1cd572b468d685
Tony Stark <stark@pop-os.localdomain>
2019-12-21 08:58:30 +0100

    updating app with shop page , collection preview component and collection item page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shop.component';
 
 
-const HatsPage = () => {
-  return <div>
-    <h1>HATS</h1>
-  </div>
-}
+
 
 
 function App(){
@@ -16,7 +13,7 @@ function App(){
     <div>
       <Switch>
       <Route exact={true} path='/' component={HomePage} />
-      <Route exact={true} path='/hats' component={HatsPage}/>
+      <Route exact={true} path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component';
+const CollectionPreview = ({title, items}) => {
+    
+return <div className='collection-preview'>
+<h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+    {
+        items.filter((item, indx)=> indx < 4 ).map(({id, ...itemProps}) => 
+        
+        (
+           <CollectionItem key={id} {...itemProps}/> 
+        )
+        )
+    }
+
+    </div>
+</div>
+
+}
+
+export default CollectionPreview;
