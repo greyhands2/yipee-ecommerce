ff4b33c0613a41ab14cd11b6e9045ae25d778164
starq <greyhands2@gmail.com>
2020-11-23 10:49:20 +0100

    cart dropdown toggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,13 @@ import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header = ({currentUser}) =>(
+const Header = ({currentUser, hidden}) =>(
 <div className='header'>
     <Link className='logo-container' to="/">
         <Logo className='logo'/>
@@ -24,13 +28,27 @@ const Header = ({currentUser}) =>(
             <Link className='option' to='/login'>
             LOG IN
             </Link>
+            
         }
         
+        <CartIcon />    
+       
+        
+        
     </div>
+    {
+        hidden ? null
+            :
+            <CartDropdown />    
+    }
+    
 </div>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+
+//nested destructuring :)
+const mapStateToProps = ({user: {currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
