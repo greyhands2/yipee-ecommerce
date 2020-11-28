3dd589a73b5e39371504b1c93a24926d8b656397
starq <greyhands2@gmail.com>
2020-11-28 08:58:45 +0100

    stripe integration and favicon

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends React.Component{
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: setCurrentUser
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = (dispatch) =>({
