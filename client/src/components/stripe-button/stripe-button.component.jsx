4fa705eb2a92de82345de4ac89d1dcbbbf1133dc
starq <greyhands2@gmail.com>
2020-12-10 14:02:35 +0100

    added stripe payment backend with node express and sectioned project to client for frontend and server for the payment restful api

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -19,8 +19,8 @@ const StripeCheckoutButton = ({price}) =>{
             }
         }).then(response=> {
             alert('Payment Successful')
-        }).catch(e=>{
-            console.log('Payment error: ', JSON.parse(e))
+        }).catch(error=>{
+            console.log('Payment error: ', JSON.parse(error))
 
             alert(
                 'There was an issue with your payment, Please make sure you use the provided credit card'
