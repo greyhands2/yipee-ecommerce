0e4936d8f88a313cad9c66f90bc3a20a239f34ae
starq <greyhands2@gmail.com>
2021-01-10 14:14:41 +0100

    fixed button and link outline

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -12,6 +12,83 @@ export const GlobalStyle = createGlobalStyle`
         }
     }
 
+.button {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+   
+}
+
+
+.button:focus {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.button:hover {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.button:visited {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.button:link {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+
+.button:active {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+
+.a {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.a:focus {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.a:hover {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+   
+}
+
+.a:visited {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+.a:link {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
+
+
+.a:active {
+  text-decoration: none;
+   outline:0 !important;
+   outline-offset: none !important;
+}
     a {
         text-decoration: none;
         color: #000000;
